42c07d113025e7bd7eb85cac863d776192b8e452
mkthewlis <mkthewlis@gmail.com>
2020-08-28 12:36:09 +0000

    feat: added services model and images for each serch service

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Stay, Relax, Eat
 
 # Register your models here.
+admin.site.register(Stay)
+admin.site.register(Relax)
+admin.site.register(Eat)
--- services/models.py
+++ services/models.py
@@ -1,32 +1,53 @@
 from django.db import models
 
 
-class Category(models.Model):
+class Stay(models.Model):
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'Room types'
 
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254,
-                                     null=True, blank=True)
+    no_of_guests = models.IntegerField(null=True, blank=True)
+    shower_bath = models.CharField(max_length=254, null=True, blank=True)
+    view = models.CharField(max_length=254, null=True, blank=True)
+    has_breakfast = models.BooleanField(default=True, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
 
-    def get_friendly_name(self):
-        return self.friendly_name
 
+class Relax(models.Model):
 
-class Stay(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True,
-                                 on_delete=models.SET_NULL)
-    product_key = models.IntegerField(null=True, blank=True)
+    class Meta:
+        verbose_name_plural = 'Spa treatments'
+
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    no_of_guests = models.IntegerField(null=True, blank=True)
-    shower_bath = models.CharField(max_length=254)
-    view = models.CharField(max_length=254)
-    breakfast = models.BooleanField(default=True, null=True, blank=True)
+    length = models.IntegerField(null=True, blank=True)
+    scents = models.CharField(max_length=254, null=True, blank=True)
+    aims = models.CharField(max_length=254, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Eat(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Menu types'
+    
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    protein = models.CharField(max_length=254, null=True, blank=True)
+    no_of_courses = models.IntegerField(null=True, blank=True)
+    has_gluten = models.BooleanField(default=True, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
