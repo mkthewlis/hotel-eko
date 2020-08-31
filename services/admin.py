4fa5b279da47081412df3eb3c482b3c6bb964eef
mkthewlis <mkthewlis@gmail.com>
2020-08-31 07:17:41 +0000

    feat: customised services admin.py

--- services/admin.py
+++ services/admin.py
@@ -1,7 +1,47 @@
 from django.contrib import admin
 from .models import Stay, Relax, Eat
 
-# Register your models here.
-admin.site.register(Stay)
-admin.site.register(Relax)
-admin.site.register(Eat)
+
+class StayAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'no_of_guests',
+        'shower_bath',
+        'view',
+        'has_breakfast',
+        'price',
+        'image',
+    )
+    ordering = ('sku',)
+
+
+class RelaxAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'length',
+        'scents',
+        'aims',
+        'price',
+        'image',
+    )
+    ordering = ('sku',)
+
+
+class EatAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'protein',
+        'no_of_courses',
+        'has_gluten',
+        'price',
+        'image',
+    )
+    ordering = ('sku',)
+
+
+admin.site.register(Stay, StayAdmin)
+admin.site.register(Relax, RelaxAdmin)
+admin.site.register(Eat, EatAdmin)
