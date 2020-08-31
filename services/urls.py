cea946a1d2261c7cb80148759fc26715ac2cae19
mkthewlis <mkthewlis@gmail.com>
2020-08-31 08:55:11 +0000

    feat: added services views and set up basic html templates for stay, relax & eat products

---
+++ services/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('stay/', views.all_stay_services, name='all_stay_services'),
+    path('relax/', views.all_relax_services, name='all_relax_services'),
+    path('eat/', views.all_eat_services, name='all_eat_services')
+]
--- services/views.py
+++ services/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from .models import Stay, Relax, Eat
 
-# Create your views here.
+
+def all_stay_services(request):
+    """ A view to return all Stay options to users """
+
+    stay = Stay.objects.all()
+
+    context = {
+        'stay': stay,
+    }
+
+    return render(request, 'services/stay.html', context)
+
+
+def all_relax_services(request):
+    """ A view to return all Relax options to users """
+
+    relax = Relax.objects.all()
+
+    context = {
+        'relax': relax,
+    }
+
+    return render(request, 'services/relax.html', context)
+
+
+def all_eat_services(request):
+    """ A view to return all Eat options to users """
+
+    eat = Eat.objects.all()
+
+    context = {
+        'eat': eat,
+    }
+
+    return render(request, 'services/eat.html', context)
