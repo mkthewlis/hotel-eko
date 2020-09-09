a9dfeaed38774490e5cb093c84947c0918a0ab35
mkthewlis <mkthewlis@gmail.com>
2020-09-09 13:31:03 +0000

    feat: began to add stripe elements with CI module videos

--- checkout/forms.py
+++ checkout/forms.py
@@ -20,12 +20,12 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
+            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County, State or Locality',
-            'country': 'Country',
+            'county': 'County',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
@@ -35,5 +35,5 @@ class OrderForm(forms.ModelForm):
             else:
                 placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
-        self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-        self.fields[field].label = False
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HHrVAH11dpiW3htUNh5qYEA4aYCQyp0MiuIjf9e2ZsWzqH8nni6hs6Li4AigKWk9LVIJmrYkMqju7vTQYxk0TU500itWKIm1O',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
