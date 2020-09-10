fde6787451e23275150e7c819a13f21318cb03a7
mkthewlis <mkthewlis@gmail.com>
2020-09-10 13:41:24 +0000

    feat: added further content to checkout and checkout_success views and created checkout success url

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from services.models import Service
 from retreat.contexts import retreat_contents
 
 import stripe
@@ -12,10 +14,54 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    retreat = request.session.get('retreat', {})
-    if not retreat:
-        messages.error(request, "You haven't added anything to your retreat yet.")
-        return redirect(reverse('all_stay_services'))
+    if request.method == 'POST':
+        retreat = request.session.get('retreat', {})
+
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in retreat.items():
+                try:
+                    service = Service.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            service=service,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                except Service.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your retreat wasn't found in \
+                            our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_retreat'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse(
+                'checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+    else:
+        retreat = request.session.get('retreat', {})
+        if not retreat:
+            messages.error(request, "You haven't added anything to your \
+                retreat yet.")
+            return redirect(reverse('all_stay_services'))
 
     current_retreat = retreat_contents(request)
     total = current_retreat['total']
@@ -37,6 +83,29 @@ def checkout(request):
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.success(request, f'Success! Your reservation has been made. \
+        Your booking reference number is {order_number} and a confirmation \
+        email will be sent to {order.email} and our booking office will \
+        get in touch with you shortly to discuss your arrival dates.')
+
+    if 'retreat' in request.session:
+        del request.session['retreat']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
