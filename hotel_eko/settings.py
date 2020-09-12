1d7ca396c442cd57c024c96ee40f3ee61cab809e
mkthewlis <mkthewlis@gmail.com>
2020-09-12 07:37:35 +0000

    feat: added class methods to webhook handler and webhook views following CI module videos

--- hotel_eko/settings.py
+++ hotel_eko/settings.py
@@ -173,3 +173,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STRIPE_CURRENCY = 'sek'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
