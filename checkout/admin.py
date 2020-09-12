31c91d0d4c32aac2496a2907cd5e1ebf134376bb
mkthewlis <mkthewlis@gmail.com>
2020-09-12 16:45:32 +0000

    feat: added order history and updated checkout views following CI module videos

--- checkout/admin.py
+++ checkout/admin.py
@@ -14,7 +14,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'total_price', 'original_retreat',
                        'stripe_pid')
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'total_price',
