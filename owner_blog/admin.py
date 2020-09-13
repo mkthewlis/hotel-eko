e7b40ac6cded1c55206fc12c52b5797768c631d1
mkthewlis <mkthewlis@gmail.com>
2020-09-13 14:43:17 +0000

    feat: added blog structure to profile include and adjusted spacing and padding for all profile templates

--- owner_blog/admin.py
+++ owner_blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import OwnerBlog
 
-# Register your models here.
+
+class OwnerBlogReview(admin.ModelAdmin):
+    """
+    Edit, add and display owner's blog posts
+    in the admin
+    """
+
+    list_display = (
+        "user_profile",
+        "title",
+        "date_added",
+        "thought_content"
+    )
+
+
+admin.site.register(OwnerBlog, OwnerBlogReview)
