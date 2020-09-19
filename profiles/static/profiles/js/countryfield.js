541a252ee85d52dda6d48bddbc88ff98a41bb20c
mkthewlis <mkthewlis@gmail.com>
2020-09-19 07:58:20 +0000

    fix: corrected color of placeholder form text throughout project. Added permanent scroll bar to profile template and checked functionality of service cards

--- profiles/static/profiles/js/countryfield.js
+++ profiles/static/profiles/js/countryfield.js
@@ -1,11 +1,11 @@
 let countrySelected = $('#id_default_country').val();
 if(!countrySelected) {
-    $('#id_default_country').css('color', '#B39F99');
+    $('#id_default_country').css('color', '#614A42');
 };
 $('#id_default_country').change(function() {
     countrySelected = $(this).val();
     if(!countrySelected) {
-        $(this).css('color', '#B39F99');
+        $(this).css('color', '#614A42');
     } else {
         $(this).css('color', '#251D1D');
     }
