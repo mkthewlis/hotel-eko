a074155a39cf4aec3e22c6b450f1db2548bdf5f4
mkthewlis <mkthewlis@gmail.com>
2020-09-23 12:22:57 +0000

    feat: added Procfile, installed gunicorn and updated allowed hosts

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hotel_eko.wsgi:application
--- hotel_eko/settings.py
+++ hotel_eko/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'a9t^y4%3!_y_55p$1+1^(#%$7td#h6uh-b0230b%w93=+kkwl3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hotel-eko.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,18 @@ WSGI_APPLICATION = 'hotel_eko.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
