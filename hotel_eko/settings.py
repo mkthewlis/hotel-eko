7a9b48004da8aa369037c22d8f22ff1e774b4c84
mkthewlis <mkthewlis@gmail.com>
2020-09-23 12:12:41 +0000

    feat: began to set up for deployment to heroku by updating requirements.txt and settings.py files

--- hotel_eko/settings.py
+++ hotel_eko/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,7 +129,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
