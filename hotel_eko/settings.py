709a9eec0fdf290d60a0441cb1a6a8e6c41e5635
mkthewlis <mkthewlis@gmail.com>
2020-09-23 13:10:57 +0000

    fix: corrected typo of AWS bucket name

--- hotel_eko/settings.py
+++ hotel_eko/settings.py
@@ -181,7 +181,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'hote-eko'
+    AWS_STORAGE_BUCKET_NAME = 'hotel-eko'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
